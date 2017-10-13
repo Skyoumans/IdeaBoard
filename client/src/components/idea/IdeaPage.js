01cbfeee0b2840a23ba87901fdfc9097e17fe0f4
Skyoumans <skyyoumans@gmail.com>
2017-10-13 10:16:32 -0400

    commit

---
+++ client/src/components/idea/IdeaPage.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import axios from 'axios'
+// import styled from 'styled-components'
+
+class IdeaPage extends Component {
+    state = {
+        user: {
+            userName: '',
+            password: '',
+            ideas: []
+        }
+    }
+
+    async componentWillMount(){
+        console.log(this.props.match.params.userId)
+        const { userId } = this.props.match.params
+        const res = await axios.get(`/api/users/${userId}`)
+        console.log(res.data)
+        this.setState({user: res.data})
+
+    }
+    render() {
+        return (
+            <div>
+                <h1>{this.state.user.userName}'s Idea Board</h1>
+                {this.state.user.ideas.map((idea) => {
+                    return (
+                        <div key={idea._id}>
+                            <h3>{idea.title}</h3>
+                            <p>{idea.description}</p>
+                        </div>
+                    )
+                })}
+            </div>
+        );
+    }
+}
+
+export default IdeaPage;
